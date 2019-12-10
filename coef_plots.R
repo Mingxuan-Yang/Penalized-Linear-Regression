9f1e370e7fe41e0b1afda4d1be7f6009549cf20d
Linlin <lilinlin@Linlins-mac.local>
2019-12-09 23:34:53 -0500

    add interactions

---
+++ coef_plots.R
@@ -0,0 +1,89 @@
+library(highcharter)
+library(dplyr)
+library(stringr)
+library(glmnet)
+
+#### to do list:
+# 1. should be able to extend the range of lambda
+# 2. add interactions checks: nrow(x) > ncol(x)
+# 3. should be able to adjust the range of lambda
+
+mutate.inters <- function(xx, interaction_way)
+{
+  return(do.call(cbind, combn(colnames(xx), interaction_way, FUN= function(x)
+    list(setNames(data.frame(xx[,x[1]]*xx[,x[2]]),
+                  paste(x, collapse=":")) ))))
+}
+
+mutate.inter <- function(xx, interactions)
+{
+  ## got this idea from https://stackoverflow.com/questions/31905221/r-generate-all-possible-interaction-variables
+  if(interactions == 1)
+  {x = xx}
+  else if(interactions == 2)
+    x = cbind(xx, mutate.inters(xx, 2))
+  else if(interactions == 3)
+  {x = cbind(xx, mutate.inters(xx, 2), mutate.inters(xx, 3))}
+  else if(interactions == 4)
+  {x = cbind(xx, mutate.inters(xx, 2), mutate.inters(xx, 3), mutate.inters(xx, 4))}
+  else
+  {
+    warning("We only capture 4-way interactions")
+    x = mutate.inter(xx, interactions = 4)
+  }
+  return(x)
+}
+
+reg <- function(df, response = 1, model, interactions = 1, lambda0 = seq(0, 100, by = 4))
+{
+  ## df:  a data frame contains covariates and response
+  ## response: which col is the response
+  ## model: "Ridge" or "Lasso"
+  ## interactions: default 0 for no interactions; 2 for all 2-way interactions;
+  ###              3 for all 3-way interactions; 4 for all 4-way interactions
+  ## lambda0: a sequence of shrinkage parameters to be considered
+  ncol <- dim(df)[2]
+  coef <- data.frame()
+  y <- df[, response]
+  xx <- df[, -response]
+  x <- mutate.inter(xx, interactions)
+  #x <- cbind(1, x)
+  #colnames(x) <- c("(Intercept)", colnames(x)[-1])
+  for(i in 1:length(lambda0)){
+    coef[i, 1:ncol] <- c(coef(glmnet(as.matrix(x), y, lambda = lambda0[i],
+                                     alpha = as.numeric(model == "Lasso")))[-1],
+                         lambda0[i])
+  }
+  colnames(coef) <- c(colnames(x), "lambda")
+  return(coef)
+}
+
+
+###### function to create an interface
+hc_plot_returns <- function(coef, name){
+  ## coef is the objective data set
+  ## name is "Ridge" or "Lasso"
+  ## lambda is a sequence of assigned shrinkage parameters, should be positive
+  
+  hc_plot <- highchart(type = "chart") %>%
+    hc_xAxis(categories = coef$lambda,
+             title = list(text = "lambda")) %>%
+    hc_yAxis(title = list(text = "coefficient")) %>%
+    hc_title(
+      text = str_c("Coefficients of <span style=\"color:#e5b13a\"> ", name, "</span> regression"),
+      style = list(fontWeight = "bold", useHTML = TRUE),
+      align = "center") %>%
+    hc_tooltip(borderWidth = 1, table = TRUE, sort = TRUE)
+  
+  for(i in 1:(dim(coef)[2] - 1)){
+    hc_plot <- hc_plot %>%
+      hc_add_series(name = colnames(coef)[i], data = coef[,i])
+  }
+  return(hc_plot)
+}
+
+hc_plot_returns_mem <- memoise::memoise(hc_plot_returns)
+
+### test
+swiss <- datasets::swiss
+hc_plot_returns_mem(reg(swiss, model = "Ridge"), "Ridge")
