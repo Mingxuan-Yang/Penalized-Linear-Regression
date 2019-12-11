d394873421eca4549909a934ceae5da701ea449f
Bo Liu <bl226@duke.edu>
2019-12-11 17:22:37 -0500

    improved version

--- plot.R
+++ plot.R
@@ -10,54 +10,38 @@ library(glmnet)
 # 3. should be able to adjust the range of lambda
 # 4. needs to improve xlab
 
-mutate.inter <- function(xx, interactions)
-{
-  ## got this idea from https://stackoverflow.com/questions/31905221/r-generate-all-possible-interaction-variables
-  if(interactions == 1)
-  {x = xx}
-  else if(interactions == 2)
-    x = cbind(xx, do.call(cbind, combn(colnames(xx), 2, FUN= function(x)
-      list(setNames(data.frame(xx[,x[1]]*xx[,x[2]]),
-                    paste(x, collapse=":")) ))))
-  else if(interactions == 3)
-  {x = cbind(xx, do.call(cbind, combn(colnames(xx), 2, FUN= function(x)
-    list(setNames(data.frame(xx[,x[1]]*xx[,x[2]]),
-                  paste(x, collapse=":")) ))), 
-    do.call(cbind, combn(colnames(xx), 3, FUN= function(x)
-      list(setNames(data.frame(xx[,x[1]]*xx[,x[2]]*xx[,x[3]]),
-                    paste(x, collapse=":")) ))))}
-  else if(interactions == 4)
-  {x = cbind(xx, do.call(cbind, combn(colnames(xx), 2, FUN= function(x)
-    list(setNames(data.frame(xx[,x[1]]*xx[,x[2]]),
-                  paste(x, collapse=":")) ))), 
-    do.call(cbind, combn(colnames(xx), 3, FUN= function(x)
-      list(setNames(data.frame(xx[,x[1]]*xx[,x[2]]*xx[,x[3]]),
-                    paste(x, collapse=":")) ))), 
-    do.call(cbind, combn(colnames(xx), 4, FUN= function(x)
-      list(setNames(data.frame(xx[,x[1]]*xx[,x[2]]*xx[,x[3]]*xx[,x[4]]),
-                    paste(x, collapse=":")) ))))}
-  else
-  {
-    warning("We only capture 4-way interactions")
-    x = mutate.inter(xx, interactions = 4)
+mutatePower <- function(df, powers) {
+  full_power <- sapply(colnames(df), function(x) powers[x])
+  transformpower <- function(x, power) {
+    if (is.na(power)) return (x)
+    if (power == 0) return(log(x))
+    else return (x^power)
   }
-  return(x)
+  transformname <- function(name, power){
+    if (is.na(power) || power == 1) return (name)
+    if (power == 0) return(paste0("log_", name))
+    else return (paste0(name, "_pwr", power))
+  }
+  res <- bind_cols(map2(df, full_power, transformpower))
+  colnames(res) <- map2(colnames(df), full_power, transformname)
+  return (res)
 }
 
-reg <- function(df, formula = NULL, response = 1, predictors = -1, interactions = 0,
-                model = c("Ridge", "Lasso"),  lambda0 = exp(seq(-5, 20, by = 1)))
+reg <- function(df, formula = NULL, response = 1, predictors = -1, interactions = 1,
+                model = c("Ridge", "Lasso"), lambda0 = exp(seq(-5, 20, by = 1)),
+                powerTransform = numeric(0))
 {
   ## df:  a data frame with covariates and response
   ## response: the number or the name of response
   ## predictor: the numbers or the names of predictors
-  ## interactions: if `formula` is not defined, a non-negative integer n indicating the level
+  ## interactions: if `formula` is not defined, a positive integer n indicating the level
   ###              of multiway interactions should be included
   ## model: "Ridge" or "Lasso"
   ## lambda0: a sequence of shrinkage parameters to be considered
   
   get_formula <- function(name_y, name_x, power){
     x_str <-paste(name_x, collapse = " + ")
-    if (power > 0)
+    if (power > 1)
       x_str <- paste0("(", x_str, ") ^ ", power)
     formula_str <- paste(name_y, "~" ,x_str, "- 1")
     return (as.formula(formula_str))
@@ -66,16 +50,16 @@ reg <- function(df, formula = NULL, response = 1, predictors = -1, interactions 
   # test validity of parameters
   
   
+  df <- df %>% mutatePower(powerTransform)
   # generate formula by interactions
   if (is.null(formula)){
     formula <- get_formula(colnames(df)[response],
                            colnames(df)[predictors],
                            interactions)
   }
-  print(formula)
   X <- model.matrix(formula, df)
   if ("(Intercept)" %in% colnames(X))
-    X <- X %>% select(-`(Intercept)`)
+    X <- X[, -1]
   Y <- df[, response]
   X_scaled <- scale(X)
   Y_scaled <- scale(Y)
@@ -85,11 +69,10 @@ reg <- function(df, formula = NULL, response = 1, predictors = -1, interactions 
   ## Suppose model %in% c("Lasso", "Ridge")
   coef <- do.call(bind_rows,
     map(lambda0, 
-        ~ glmnet(X_scaled, Y_scaled, lambda = .x, alpha = ifelse(model == "Lasso", 1, 0)) %>%
-          coef() %>% .[-1] %>% set_names(colnames(X_scaled)))
+        ~ c(glmnet(X_scaled, Y_scaled, lambda = .x, alpha = ifelse(model == "Lasso", 1, 0)) %>%
+            coef() %>% .[-1] %>% set_names(colnames(X_scaled)), "lambda" = .x))
   )
-  print(coef)
-  return(coef)
+  return(as.data.frame(coef))
 }
 
 
@@ -121,4 +104,8 @@ hc_plot_returns_mem <- memoise::memoise(hc_plot_returns)
 
 ### test
 swiss <- datasets::swiss 
-hc_plot_returns_mem(reg(swiss, model = "Ridge"), "Ridge")
+hc_plot_returns_mem(reg(swiss, model = "Lasso"), "Lasso")
+hc_plot_returns_mem(reg(swiss, interactions = 2, model = "Ridge"), "Ridge")
+hc_plot_returns_mem(reg(swiss, formula = Agriculture ~ Education + log(Catholic), model = "Lasso"), "Lasso")
+hc_plot_returns_mem(reg(swiss, model = "Lasso",
+                        powerTransform = c(Education = 2, Catholic = 0)), "Lasso")
